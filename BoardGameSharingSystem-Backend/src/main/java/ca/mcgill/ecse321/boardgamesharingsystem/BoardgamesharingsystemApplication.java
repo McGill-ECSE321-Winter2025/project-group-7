618f369953bb82a01aeb949b8a10ba3b8e2f70c2
snowqc <romyburn0@gmail.com>
2025-02-03 14:32:34 -0500

    Create starter Spring Boot Project

---
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/BoardgamesharingsystemApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.boardgamesharingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamesharingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgamesharingsystemApplication.class, args);
+	}
+
+}
---
+++ BoardGameSharingSystem-Backend/src/test/java/ca/mcgill/ecse321/boardgamesharingsystem/BoardgamesharingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.boardgamesharingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamesharingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
