91b4273288900469bc5deadba9924fe264d38f32
DontDieThankYou <96702893+DontDieThankYou@users.noreply.github.com>
2025-02-07 14:12:04 -0500

    Adding Event Class

---
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/Event.java
@@ -0,0 +1,64 @@
+package ca.mcgill.ecse321.boardgamesharingsystem.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Event {
+    private int id;
+    private Date startDate;
+    private Time startTime;
+    private Date endDate;
+    private Time endTime;
+    private int maxNumParticipants;
+    private String location;
+    private String description;
+    private UserAccount creator;
+
+    public Event(Date startDate, Time startTime, Date endDate, Time endTime, int maxNumParticipants, String location, String description, UserAccount creator)
+    {
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+        this.maxNumParticipants = maxNumParticipants;
+        this.location = location;
+        this.description = description;
+        this.creator = creator;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public int getStartDate()
+    {
+        return startDate;
+    }
+    public int getStartTime()
+    {
+        return startTime;
+    }
+    public int getEndDate()
+    {
+        return endDate;
+    }
+    public int getEndTime()
+    {
+        return endTime;
+    }
+    public int getMaxNumParticipants()
+    {
+        return maxNumParticipants;
+    }
+    public int getLocation()
+    {
+        return location;
+    }
+    public int getDescription()
+    {
+        return description;
+    }
+    public UserAccount getCreator()
+    {
+        return creator;
+    }
+}
