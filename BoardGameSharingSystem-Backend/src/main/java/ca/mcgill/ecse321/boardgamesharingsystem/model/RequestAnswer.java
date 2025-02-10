7f9e8e8ebb8e51be45bfa175da25959af83739d1
sen-snigdha <snigdha.dawson@gmail.com>
2025-02-10 15:07:13 -0500

    Update RequestAnswer with right annotations

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/RequestAnswer.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/RequestAnswer.java
@@ -3,15 +3,26 @@ package ca.mcgill.ecse321.boardgamesharingsystem.model;
 import java.sql.Date;
 import java.sql.Time;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 /**
  * A class that defines the game owner's answer of the request
  */
 
+@Entity
 public class RequestAnswer {
+    @Id
+    @GeneratedValue
     private int id;
     private Date dropOffDate;
     private Time dropOffTime;
     private String location;
+
+    @OneToOne
+    private BorrowRequest request;
     private String contactEmail;
 
 
