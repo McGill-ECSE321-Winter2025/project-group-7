4d99cb84aac34a257b8b6335f672a0f508994f92
Shengyi Z <shengyi.zhong@mail.mcgill.ca>
2025-02-10 08:11:25 -0500

    update registration & gamecopy

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/GameCopy.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/GameCopy.java
@@ -1,4 +1,52 @@
 package ca.mcgill.ecse321.boardgamesharingsystem.model;
 
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
 public class GameCopy {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    private Game game;
+    @ManyToOne
+    private GameOwner owner;
+    @OneToMany
+    private Set<BorrowRequest> requests;
+
+    public GameCopy() {
+
+    }
+
+    public GameCopy(Game game) {
+        this.game = game;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Game getGame() {
+        return game;
+    }
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    public GameOwner getGameOwner() {
+        return gameOwner;
+    }
+
+    public void setGameOwner(GameOwner gameOwner) {
+        this.gameOwner = gameOwner;
+    }
+    public Set<BorrowRequest> getRequests() {
+        return requests;
+    }
+    public void setRequests(Set<BorrowRequest> requests) {
+        this.requests = requests;
+    }
 }
--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/Registration.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/model/Registration.java
@@ -1,4 +1,48 @@
 package ca.mcgill.ecse321.boardgamesharingsystem.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Registration {
+
+    @Id
+    @GeneratedValue
+    private LocalDateTime registrationDateTime;
+    @ManyToOne
+    private UserAccount user;
+    @ManyToOne
+    private Event event;
+
+    public Registration() {
+
+    }
+    public Registration(LocalDateTime registrationDateTime, UserAccount user, Event event) {
+        this.registrationDateTime = registrationDateTime;
+        this.user = user;
+        this.event = event;
+    }
+    public LocalDateTime getRegistrationDateTime() {
+        return registrationDateTime;
+    }
+
+    public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
+        this.registrationDateTime = registrationDateTime;
+    }
+
+    public UserAccount getUser() {
+        return user;
+    }
+
+    public void setUser(UserAccount user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
