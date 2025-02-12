226d02ae6f158a5eaeab5eeb37c76f89258d62e9
DontDieThankYou <96702893+DontDieThankYou@users.noreply.github.com>
2025-02-12 14:05:49 -0500

    Create Event Crud Repository

---
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/EventRepository.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse321.boardgamesharingsystem.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.boardgamesharingsystem.model.Event;
+
+public interface EventRepository extends CrudRepository<Event, Integer> {
+	public Event findEventById(int id);
+}
