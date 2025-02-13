5abe10bd7711b3da5bb37d52f27e10a83877990e
artimirchi <156237712+artimirchi@users.noreply.github.com>
2025-02-12 22:22:36 -0500

    create review and game owner repo tests

---
+++ BoardGameSharingSystem-Backend/src/test/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/GameOwnerRepositoryTests.java
@@ -0,0 +1,55 @@
+package ca.mcgill.ecse321.boardgamesharingsystem.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.boardgamesharingsystem.model.GameOwner;
+import ca.mcgill.ecse321.boardgamesharingsystem.model.UserAccount;
+
+@SpringBootTest
+public class GameOwnerRepositoryTests {
+    @Autowired
+    private GameOwnerRepository gameOwnerRepository;
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @AfterEach
+    public void clearDatabase(){
+        gameOwnerRepository.deleteAll();
+        userAccountRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistAndLoadGameOwner()
+    {
+        //arrange
+        String name = "bojack";
+        String email = "bojack@gmail.com";
+        String password = "password";
+        UserAccount currentUser = new UserAccount(name, email, password);
+        userAccountRepository.save(currentUser);
+
+        GameOwner gameOwner = new GameOwner(currentUser);
+        gameOwnerRepository.save(gameOwner);
+
+        //act
+        GameOwner gameOwnerFromDb = gameOwnerRepository.findGameOwnerById(gameOwner.getId());
+
+        //assert
+        assertNotNull(gameOwnerFromDb);
+        assertEquals(gameOwner.getId(), gameOwnerFromDb.getId());
+        assertNotNull(gameOwnerFromDb.getUser());
+        assertEquals(gameOwnerFromDb.getUser().getId(), currentUser.getId());
+
+    }
+
+
+
+
+}
