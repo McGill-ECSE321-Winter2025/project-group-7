4a140b5749101be7a46875d2cd1fb7f29ad4a83b
snowqc <romyburn0@gmail.com>
2025-03-10 13:13:09 -0400

    added findAll to GameRepository.java

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/GameRepository.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/GameRepository.java
@@ -1,9 +1,12 @@
 package ca.mcgill.ecse321.boardgamesharingsystem.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.boardgamesharingsystem.model.Game;
 
 public interface GameRepository extends CrudRepository<Game,Integer> {
     public Game findGameById(int id);
+    public List<Game> findAll();
 }
