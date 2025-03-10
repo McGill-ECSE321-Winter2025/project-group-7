bfd9904f8afefcefd9542e4afb1d7631f7d28ae9
josiee17 <146299974+josiee17@users.noreply.github.com>
2025-03-10 14:46:27 -0400

    Update ReviewService.java with missing annotation

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/service/ReviewService.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/service/ReviewService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import ca.mcgill.ecse321.boardgamesharingsystem.model.Game;
@@ -16,6 +17,7 @@ import ca.mcgill.ecse321.boardgamesharingsystem.repo.ReviewRepository;
 import ca.mcgill.ecse321.boardgamesharingsystem.repo.UserAccountRepository;
 import jakarta.transaction.Transactional;
 
+@Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepo;
