a78a2eaf1e9463949be8a706ec52e4c74b9ecd72
snowqc <romyburn0@gmail.com>
2025-03-12 18:47:39 -0400

    Renamed GameCopiesController.java to GameCopyController.java

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/controller/GameCopyController.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/controller/GameCopyController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("gameCopies")
-public class GameCopiesController {
+public class GameCopyController {
 
     @Autowired
     private GameOwningService gameOwningService;
