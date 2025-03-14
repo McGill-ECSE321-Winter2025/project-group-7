b035fadfc666674808b8aee433bbdcbd8afe2183
Doddy <96702893+DontDieThankYou@users.noreply.github.com>
2025-03-14 14:36:57 -0400

    Update EventGameResponseDto.java, RegistrationResponseDto.java To: include information on the registration/eventGame's associations

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/dto/EventGameResponseDto.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/dto/EventGameResponseDto.java
@@ -5,13 +5,31 @@ import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import ca.mcgill.ecse321.boardgamesharingsystem.model.Event;
 import ca.mcgill.ecse321.boardgamesharingsystem.model.EventGame;
+import ca.mcgill.ecse321.boardgamesharingsystem.model.Game;
 
 public class EventGameResponseDto {
     private int eventId;
     private int gameId;
     private Date responseDate;
     private Time responseTime;
+
+    private Date eventStartDate;
+    private Time eventStartTime;
+    private Date eventEndDate;
+    private Time eventEndTime;
+    private int eventMaxNumParticipants;
+    private String eventLocation;
+    private String eventDescription;
+    private String eventContactEmail;
+    private int eventCreatorId;
+
+    private String gameTitle;
+    private int gameMinNumPlayers;
+    private int gameMaxNumPlayers;
+    private String gamePictureURL;
+    private String gameDescription;
 
     //No args constructor for Jackson
     @SuppressWarnings("unused")
@@ -26,6 +44,24 @@ public class EventGameResponseDto {
         this.gameId = eventGame.getKey().getGame().getId();
         responseDate = Date.valueOf(LocalDate.now());
         responseTime = Time.valueOf(LocalTime.now());
+
+        Event event = eventGame.getKey().getEvent();
+        this.eventStartDate = event.getStartDate();
+        this.eventStartTime = event.getStartTime();
+        this.eventEndDate = event.getEndDate();
+        this.eventEndTime = event.getEndTime();
+        this.eventMaxNumParticipants = event.getMaxNumParticipants();
+        this.eventLocation = event.getLocation();
+        this.eventDescription = event.getDescription();
+        this.eventContactEmail = event.getContactEmail();
+        this.eventCreatorId = event.getCreator().getId();
+
+        Game game = eventGame.getKey().getGame();
+        this.gameTitle = game.getTitle();
+        this.gameMinNumPlayers = game.getMinNumPlayers();
+        this.gameMaxNumPlayers = game.getMaxNumPlayers();
+        this.gamePictureURL = game.getPictureURL();
+        this.gameDescription = game.getDescription();
     }
 
     public int getEventId() {
@@ -43,4 +79,60 @@ public class EventGameResponseDto {
     public Time getResponseTime() {
         return responseTime;
     }    
+
+    public Date getEventStartDate() {
+        return eventStartDate;
+    }
+    
+    public Time getEventStartTime() {
+        return eventStartTime;
+    }
+    
+    public Date getEventEndDate() {
+        return eventEndDate;
+    }
+    
+    public Time getEventEndTime() {
+        return eventEndTime;
+    }
+    
+    public int getEventMaxNumParticipants() {
+        return eventMaxNumParticipants;
+    }
+    
+    public String getEventLocation() {
+        return eventLocation;
+    }
+    
+    public String getEventDescription() {
+        return eventDescription;
+    }
+    
+    public String getEventContactEmail() {
+        return eventContactEmail;
+    }
+    
+    public int getEventCreatorId() {
+        return eventCreatorId;
+    }
+
+    public String getGameTitle() {
+        return gameTitle;
+    }
+    
+    public int getGameMinNumPlayers() {
+        return gameMinNumPlayers;
+    }
+    
+    public int getGameMaxNumPlayers() {
+        return gameMaxNumPlayers;
+    }
+    
+    public String getGamePictureURL() {
+        return gamePictureURL;
+    }
+    
+    public String getGameDescription() {
+        return gameDescription;
+    }
 }
