18a9057aada183c319b0e0fe60b6b88cd7dd1247
snowqc <romyburn0@gmail.com>
2025-03-14 11:57:39 -0400

    Added fields to GameCopyResponseDto.java

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/dto/GameCopyResponseDto.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/dto/GameCopyResponseDto.java
@@ -6,18 +6,50 @@ public class GameCopyResponseDto {
     private int id;
     private int gameId;
     private int ownerId;
+    private String gameDescription;
+    private int gameMaxNumPlayers;
+    private int gameMinNumPlayers;
+    private String gamePicture;
+    private String gameTitle;
 
     public GameCopyResponseDto(GameCopy gameCopy) {
         this.id = gameCopy.getId();
         this.gameId = gameCopy.getGame().getId();
         this.ownerId = gameCopy.getGameOwner().getId();
+        this.gameDescription = gameCopy.getGame().getDescription();
+        this.gameMaxNumPlayers = gameCopy.getGame().getMaxNumPlayers();
+        this.gameMinNumPlayers = gameCopy.getGame().getMinNumPlayers();
+        this.gamePicture = gameCopy.getGame().getPictureURL();
+        this.gameTitle = gameCopy.getGame().getTitle();
+
     }
 
-    public int getId() { return id; }
-    public int getGameId() { return gameId; }
-    public int getOwnerId() { return ownerId; }
+    public int getId() {
+        return id;
+    }
+    public int getGameId() {
+        return gameId;
+    }
+    public int getOwnerId() {
+        return ownerId;
+    }
 
-    public void setId(int id) { this.id = id; }
-    public void setGameId(int gameId) { this.gameId = gameId; }
-    public void setOwnerId(int ownerId) { this.ownerId = ownerId; }
+    public String getGameDescription() {
+        return gameDescription;
+    }
+
+    public int getGameMaxNumPlayers() {
+        return gameMaxNumPlayers;
+    }
+
+    public int getGameMinNumPlayers() {
+        return gameMinNumPlayers;
+    }
+
+    public String getGamePicture() {
+        return gamePicture;
+    }
+    public String getGameTitle() {
+        return gameTitle;
+    }
 }
