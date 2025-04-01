59616580088df732b64210283760bde647ef8453
snowqc <romyburn0@gmail.com>
2025-04-01 15:46:23 -0400

    Implemented basic GamesView

--- BoardGameSharingSystem-Frontend/src/views/GamesView.vue
+++ BoardGameSharingSystem-Frontend/src/views/GamesView.vue
@@ -5,7 +5,7 @@ import { Button } from 'primevue';
 </script>
 
 <template>
-  <!-- <h1 class="game-title">Games</h1> -->
+  <h1 class="game-title">Browse Games</h1>
   <div class="game-card-wrapper">
     <div class="game-card-content">
       <div class="flip-card">
@@ -18,43 +18,17 @@ import { Button } from 'primevue';
     </Card>
     </div>
     <div class="flip-card-back">
-      <Card style="width: 20rem; overflow: hidden" class="game-card">
-        <template #header>
-        </template>
-        <template #title>Game</template>
-        <template #subtitle>Min-Max Players </template>
+      <Card style="width: 20rem; overflow: hidden" class="game-card game-card-back">
+        <template #title class="game-card-title">Game Title</template>
         <template #content>
-            <p class="m-0">
-              This is a short description about the game 
+          <p class="card-text">Min # Players:</p>
+          <p class="card-text-center">Min # Players:</p>
+          <p class="card-text">Max # Players:</p>
+          <p class="card-text-center">Min # Players:</p>
+            <p class="description">
+              This is a short description about the game and it is getting increasingly longer so i have to deal with it 
             </p>
-            <Button>View Details</Button>
-        </template>
-    </Card>
-    </div>
-        </div>
-      </div>
-    </div>  
-    <div class="game-card-content">
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-    <Card style="width: 20rem; overflow: hidden" class="game-card">
-        <template #header>
-          <img src="/test-image2.jpg" alt="game image1" class="game-images">
-        </template>
-    </Card>
-    </div>
-    <div class="flip-card-back">
-      <Card style="width: 20rem; overflow: hidden" class="game-card">
-        <template #header>
-        </template>
-        <template #title>Game</template>
-        <template #subtitle>Min-Max Players </template>
-        <template #content>
-            <p class="m-0">
-              This is a short description about the game 
-            </p>
-            <Button>View Details</Button>
+            <Button class="details-button">View Details</Button>
         </template>
     </Card>
     </div>
@@ -125,6 +99,60 @@ import { Button } from 'primevue';
 .game-card{
   height: 300px ;
 }
+
+.game-images{
+  width: 100%;
+  max-width: 20rem;
+  height: auto;
+  object-fit: cover;
+}
+
+.game-card-back{
+  background-color: #602809;
+  font-family: "Mansalva", sans-serif;
+  color: beige;
+}
+
+.card-text{
+  text-align: left;
+}
+
+.details-button{
+  margin-top: 10vh;
+  background-color:transparent ;
+  border-color: white;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+}
+
+.details-button:hover{
+  background-color:beige;
+  color: beige;
+}
+
+.card-text-center{
+  text-align: center;
+}
+
+.game-title{
+  color: #602809;
+  font-family:  "Mansalva", sans-serif;
+  margin-top: 17.5vh;
+  background-color: rgba(221, 216, 201, 0.7);
+  border-radius: 5%;
+  border-top-width: 0;
+}
+
+.description{
+  text-align: left;
+  height: 15%;
+}
+
+.game-card-title{
+  height: 10%;
+}
+
 
 
 </style>
