9c2b69065f2a394b0c35075a2bda4663ed741570
Doddy <96702893+DontDieThankYou@users.noreply.github.com>
2025-04-01 19:59:42 -0400

    Add CrossOrigin Annotation to RegistrationController.java

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/controller/RegistrationController.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import ca.mcgill.ecse321.boardgamesharingsystem.dto.RegistrationResponseDto;
 import ca.mcgill.ecse321.boardgamesharingsystem.model.Registration;
 import ca.mcgill.ecse321.boardgamesharingsystem.service.EventService;
 
+@CrossOrigin(origins = "http://localhost:8090")
 @RestController
 public class RegistrationController {
     @Autowired
