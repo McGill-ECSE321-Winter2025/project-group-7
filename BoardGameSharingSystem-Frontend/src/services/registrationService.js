1c93408368ba995b69ff2bef72939968016da10d
Doddy <96702893+DontDieThankYou@users.noreply.github.com>
2025-04-01 20:00:49 -0400

    Fix error required Type Object for registrationService.js

--- BoardGameSharingSystem-Frontend/src/services/registrationService.js
+++ BoardGameSharingSystem-Frontend/src/services/registrationService.js
@@ -4,7 +4,7 @@ export const registrationService = {
   // GET /registrations/fromEvent
   async findRegistrationByEvent(eventId) {
     try {
-      const response = await api.get('/registrations/fromEvent', {params: eventId})
+      const response = await api.get('/registrations/fromEvent', {params: { eventId }})
       return response.data
     } catch (error) {
       console.error('Error fetching registrations:', error)
