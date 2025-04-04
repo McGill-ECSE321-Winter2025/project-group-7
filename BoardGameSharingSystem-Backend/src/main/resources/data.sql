2bab2e3c98707b707afa0c6a8126c7f7ea73f936
snowqc <romyburn0@gmail.com>
2025-04-04 17:07:02 -0400

    Added data.sql for games and modified application.properties

---
+++ BoardGameSharingSystem-Backend/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO game (id,title, min_num_players, max_num_players, pictureurl, description) 
+VALUES 
+    (1,'Monopoly', 2, 7, 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Monopoly_board_on_white_bg.jpg/1200px-Monopoly_board_on_white_bg.jpg', 'A classic board game where players buy and trade properties.'),
+    (2,'Risk', 2, 6, 'https://upload.wikimedia.org/wikipedia/en/3/36/Risk_logo.jpg', 'A strategy game where players control armies and conquer territories.'),
+    (3,'Clue', 3, 6, 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Cluedo_Clue_pack_logo.png', 'A mystery game where players solve a crime by guessing the suspect, weapon, and location.'),
+    (4,'Catan', 3, 4, 'https://upload.wikimedia.org/wikipedia/en/a/a3/Catan-2015-boxart.jpg', 'A game of resource management and settlement building.'),
+    (5,'Ticket to Ride', 2, 5, 'https://upload.wikimedia.org/wikipedia/en/9/92/Ticket_to_Ride_Board_Game_Box_EN.jpg', 'A railway-themed board game where players compete to complete train routes.')
+;
