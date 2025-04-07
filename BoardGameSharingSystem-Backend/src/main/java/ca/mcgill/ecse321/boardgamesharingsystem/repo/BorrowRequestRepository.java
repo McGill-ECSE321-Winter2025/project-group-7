126366d014089980c8d57f55aa297193ec786006
Doddy <96702893+DontDieThankYou@users.noreply.github.com>
2025-04-06 22:21:32 -0400

    fix unable to delete gamecopy that has a borrowRequest

--- BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/BorrowRequestRepository.java
+++ BoardGameSharingSystem-Backend/src/main/java/ca/mcgill/ecse321/boardgamesharingsystem/repo/BorrowRequestRepository.java
@@ -1,15 +1,15 @@
 package ca.mcgill.ecse321.boardgamesharingsystem.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.boardgamesharingsystem.model.BorrowRequest;
-
-import java.util.List;
 
 public interface BorrowRequestRepository extends CrudRepository<BorrowRequest, Integer>{
     public BorrowRequest findBorrowRequestById(int id);
     public List<BorrowRequest> findByBorrowerId(int borrowerId);
     public List<BorrowRequest> findByGameCopyId(int gameCopyId);
-
+    public void deleteByGameCopyId(int gameCopyId);
     public List<BorrowRequest> findAll();
 }
